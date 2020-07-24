1dce622f42269692a73b2a267f1d785030cda006
ViktarDzerauniuk <as.35.2011@gmail.com>
2020-07-24 17:49:41 +0300

    ux fix 3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// sctroll to top
 arrowTop.onclick = function () {
   window.scrollTo(pageXOffset, 0);
   // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
@@ -6,27 +7,8 @@ window.addEventListener('scroll', function () {
   arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
 });
 
-//по умолчанию активен первый твит
-let slideIndex = 1;
-showSlides(slideIndex);
-function currentTwit(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slider__content");
-  let dots = document.getElementsByClassName("slider__dot");
-  //все остальные твиты скрыты
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
 
-/* Open/close overlay navigation */
+// Open/close overlay navigation
 function openNav() {
   document.getElementById("top-navigation").classList.toggle("active");
   document.getElementById("menu-icon__top").classList.toggle("active");
@@ -36,9 +18,23 @@ function openNav() {
 
 // уменьшение размера навигации при скролле
 window.addEventListener('scroll', function () {
-  if (pageYOffset > 99) {
+  if (pageYOffset > 299) {
     document.getElementById("header__logo").classList.add('scrolled');
   } else {
     document.getElementById("header__logo").classList.remove('scrolled');
   }
+});
+
+// owl carousel twitter
+$(document).ready(function () {
+  const slider = $("#slider").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  });
 });
